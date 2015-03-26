00cf6fa110760624e18d5acf126fe3880760f7c0
Peter Foster <p.foster2@gmail.com>
2015-03-26 11:18:54 +0000

    Loggified all the BS2009_Eqn30_ZTApprox() in supertree calcs.  Now it does not have that upper limit of 152 or so on the number of taxa in the input trees.

--- p4/version.py
+++ p4/version.py
@@ -1,6 +1,6 @@
 versionNumberString = '0.93'   # a string that can be turned into a number
 versionNumber = float(versionNumberString)
-versionNumberModifier = ' [2015-03-20]'    # a string
+versionNumberModifier = ' [2015-03-26]'    # a string
 versionString = '%s%s' % (versionNumberString, versionNumberModifier)
-dateString = "20 March, 2015"
+dateString = "26 March, 2015"
 
